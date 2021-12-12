55b8119c0da3730323bdb750063af2c6d3d176ca
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2021-12-12 13:29:27 -0800

    simplify main method.

--- interpreter/Interpreter.java
+++ interpreter/Interpreter.java
@@ -1,44 +1,42 @@
 package interpreter;
 
 import java.io.*;
+
 import interpreter.debugger.Debugger;
 
 public class Interpreter {
 
-  protected ByteCodeLoader byteCodeLoader;
-  protected VirtualMachine vm;
+    protected ByteCodeLoader byteCodeLoader;
+    protected VirtualMachine vm;
 
-  public Interpreter(String codeFile) {
-    try {
-      CodeTable.init();
-      byteCodeLoader = new ByteCodeLoader(codeFile);
-    } catch (IOException e) {
-      System.out.println("**** " + e);
-    }
-  }
-
-  public void run() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-    Program program = byteCodeLoader.loadCodes();
-    vm = new VirtualMachine(program);
-    vm.executeProgram();
-  }
-
-  public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-    if (args.length == 0 || args.length > 2) {
-      System.out.println("*** Incorrect usage, try: java interpreter.Interpreter <file>");
-      System.exit(1);
+    public Interpreter(String codeFile) {
+        try {
+            CodeTable.init();
+            byteCodeLoader = new ByteCodeLoader(codeFile);
+        } catch (IOException e) {
+            System.out.println("**** " + e);
+        }
     }
 
-    if(args.length == 2 && !args[0].equals("-d")) {
-      // TODO: Check for ex basefilename is like samples_files/factorial and we can check for the existence of both .x and .x.cod files
-      System.out.println("*** Incorrect usage, try: java interpreter.Interpreter -d <basefilename>");
-      System.exit(1);
+    public void run() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Program program = byteCodeLoader.loadCodes();
+        vm = new VirtualMachine(program);
+        vm.executeProgram();
     }
 
-    if(args.length == 2 && args[0].equals("-d")) {
-      (new Debugger(args[1])).run();
-    } else {
-      (new Interpreter(args[0])).run();
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        if (args.length == 0 || args.length > 2) {
+            System.out.println("*** Incorrect usage, try: java interpreter.Interpreter <file>");
+            System.exit(1);
+        } else if (args.length == 1) {
+            (new Interpreter(args[0])).run();
+        } else if (args.length == 2) {
+            if (args[0].equals("-d")) {
+                (new Debugger(args[1])).run();
+            } else {
+                System.out.println("*** Incorrect usage, try: java interpreter.Interpreter -d <basefilename>");
+                System.exit(1);
+            }
+        }
     }
-  }
 }
